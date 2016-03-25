b6b807238241238149e694740d8518f672ef2b02
Alenka <ei@everblood.org>
2016-03-25 16:35:18 +0300

    Assignment for responsive web design is hals done.

--- responsive-website-development-and-design/2-responsive-web-design/4-week-res-web-bas/4-week-asgm/Assignment/assignment.js
+++ responsive-website-development-and-design/2-responsive-web-design/4-week-res-web-bas/4-week-asgm/Assignment/assignment.js
@@ -1,1 +1,65 @@
 // put your javascript code here
+
+var category_template, animals_template;
+
+var current_category = animals_data.category[0];
+var current_animals = current_category.animals[0];
+
+function showNav(template, data) {
+    var html = template(data);
+    $("#animals-tab").html(html);
+}
+
+function showTemplate(template, data) {
+    var html = template(data);
+    $("#content").html(html);
+}
+
+$(document).ready(function () {
+    var source = $("#category-template").html();
+    category_template = Handlebars.compile(source);
+
+    source = $("#animals-template").html();
+    animals_template = Handlebars.compile(source);
+
+    showNav(category_template, animals_data);
+
+    $("#Reptiles").click(function () {
+
+        var index = $(this).data("id");
+        current_category = animals_data.category[index];
+
+        $(".nav-tabs .active").removeClass("active");
+
+        $("#Reptiles").addClass("active");
+
+        showTemplate(animals_template, current_category);
+    });
+    $("#Mammals").click(function () {
+
+        var index = $(this).data("id");
+        current_category = animals_data.category[index];
+
+        $(".nav-tabs .active").removeClass("active");
+
+        $("#Reptiles").addClass("active");
+
+        showTemplate(animals_template, current_category);
+        console.log("mammals");
+    });
+    $("#Birds").click(function () {
+
+        var index = $(this).data("id");
+        current_category = animals_data.category[index];
+
+        $(".nav-tabs .active").removeClass("active");
+
+        $("#Reptiles").addClass("active");
+
+        showTemplate(animals_template, current_category);
+    });
+
+    $("#Reptiles").click();
+
+
+});
