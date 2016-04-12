10723972413b26a1a86706e6d389b14b1fb88b92
alenka.s <alenka.s@me.com>
2016-04-12 23:21:22 +0300

    Question-asnwer functionality added

---
+++ responsive-website-development-and-design/1-responsive-website-basics/4-week-res-web-dev-des/js/data.js
@@ -0,0 +1,21 @@
+var data = [
+	{
+		"id": 0,
+		"question": "choose a question"
+	},
+	{
+		"id": 1,
+		"question": "what's your age?",
+		"answer": "i'm not telling you"
+	},
+	{
+		"id": 2,
+		"question": "what's your fav color?",
+		"answer": "Mm.. white, probably"
+	},
+	{
+		"id": 3,
+		"question": "what's your real name?",
+		"answer": "You didn't expect that I would tell you that, did ya?:)"
+	}
+]
