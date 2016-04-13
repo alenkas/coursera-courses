8c9ce4fa3767c221189d3beacfd74daab5d5a3fd
Alenka <ei@everblood.org>
2016-04-13 10:51:01 +0300

    Menu linkg bugs with loading data is fixed

--- responsive-website-development-and-design/1-responsive-website-basics/4-week-res-web-dev-des/js/main.js
+++ responsive-website-development-and-design/1-responsive-website-basics/4-week-res-web-dev-des/js/main.js
@@ -1,48 +1,33 @@
 $(function() {
     function changeContent(templ) {
-    	// Hide previous content before loading new one
+        // Hide previous content before loading new one
         $("#content").children("div").css("display", "none");
         // Make requested content visible to user
         templ.style.display = "block";
         $("#content").append(templ);
     }
 
-    // function clickedMenuItem(element){
-    //     // Get element by data-link attribute
-    //     var link = $(element).data("link");
-    //     // When any link in menu nav is clicked open its content
-    //     $(element).click(function() {
-    //         // Get template by id
-    //         var template = document.getElementById(link + "_templ");
-    //         // Pass template html data to function so that it is shown
-    //         // to user
-    //         changeContent(template);
-    //     });
-    // }
-
-    $(".nav-pills").find("a").click({
-        // clickedMenuItem(this);
-        console.log(this);
-        // Get element by data-link attribute
+    $(".nav-pills").find("a").click(function() {
+        //clickedMenuItem(this);
+        //Get element by data-link attribute
         var link = $(this).data("link");
         // When any link in menu nav is clicked open its content
-        $(this).click(function() {
-            // Get template by id
-            var template = document.getElementById(link + "_templ");
-            // Pass template html data to function so that it is shown
-            // to user
-            changeContent(template);
-        });
+        // Get template by id
+        var template = document.getElementById(link + "_templ");
+        // Pass template html data to function so that it is shown
+        // to user
+        changeContent(template);
     });
 
-    // Imitate that first link in menu is clicked on load
-    $(".nav-pills").find("li:first-child a").click();    
+    // Simulate that first link in menu to be clicked on load
+    $(".nav-pills").find("li:first-child a").trigger("click");
+
     // Get element by its id
     var select_questions = document.getElementById("questions");
     var create_option;
     // for each item in the array create an option with its unique
     // id and unique text
-    for(var i = 0; i < data.length; i++){
+    for (var i = 0; i < data.length; i++) {
         create_option = document.createElement("option");
         // set unique id for each item
         create_option.setAttribute("value", data[i].id);
@@ -55,14 +40,14 @@ $(function() {
 
     // Each time user select any option from questions list this 
     // function is called
-    $("#questions").change(function(e){
+    $("#questions").change(function() {
         var $my_answer = $("#my-answer");
         // get value from selected option and turn it into number 
         // (by default it is a string)
-        var $option_value = parseInt($("#questions option:selected").val());
+        var $option_value = parseInt($("#questions").find("option:selected").val());
         // Works the same way as if statement - if user chose question one
         // then show this answer
-        switch($option_value){
+        switch ($option_value) {
             case 0:
                 $my_answer.html(data[$option_value].answer);
                 break;
@@ -77,5 +62,5 @@ $(function() {
                 break;
         }
     });
-    
+
 });
