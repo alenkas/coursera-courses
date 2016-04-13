11c8c21aa1d401c69735f9a32ce06f8975adfe85
Alenka <ei@everblood.org>
2016-04-13 15:32:51 +0300

    Hometask finished

--- responsive-website-development-and-design/1-responsive-website-basics/4-week-res-web-dev-des/js/data.js
+++ responsive-website-development-and-design/1-responsive-website-basics/4-week-res-web-dev-des/js/data.js
@@ -16,11 +16,52 @@ var data = [
 		"answer": "You didn't expect that I would tell you that, did ya?:)"
 	},
 	{
-		"question": "bla",
-		"answer": "bla"
+		"question": "Why JS?",
+		"answer": "Historically happened so:) Started with html and this went too far, ahah)"
 	},
 	{
 		"question": "Who is that boy on picture to the right?",
 		"answer": "It's Killua. He appears in 'Huntex x Hunter' series. Highly recommended for watching."
+	},
+	{
+		"question": "What else to ask?",
+		"answer": "Okay, enough questions for today."
+	},
+	{
+		"question": "Just one last question",
+		"answer": "Go ahead"
 	}
-]
+];
+
+var courses = [
+	{
+		"name": "Responsive Website Basics: Code with HTML, CSS, and JavaScript",
+		"uni": "University of London & Goldsmiths, University of London",
+		"img_src": "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/5d/056e103cf111e5a191b717b8272db1/MOOC-1-V-5.png?auto=format&dpr=1&h=72&w=72&fit=fill&bg=FFF",
+		"url": "https://www.coursera.org/learn/website-coding/home/welcome"
+	},
+	{
+		"name": "Introduction to Meteor.js Development",
+		"uni": "University of London & Goldsmiths, University of London",
+		"img_src": "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/6a/e2aa603cf211e5a922adc5eaf6c043/MOOC-3.png?auto=format&dpr=1&h=72&w=72&fit=fill&bg=FFF",
+		"url": "https://www.coursera.org/learn/meteor-development/home/welcome"
+	},
+	{
+		"name": "Web Application Development with JavaScript and MongoDB",
+		"uni": "University of London & Goldsmiths, University of London",
+		"img_src": "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/bf/d0c2f03cf211e5a922adc5eaf6c043/MOOC-4.png?auto=format&dpr=1&h=72&w=72&fit=fill&bg=FFF",
+		"url": "https://www.coursera.org/learn/web-application-development/home/welcome"
+	},
+	{
+		"name": "Responsive Website Tutorial and Examples",
+		"uni": "University of London & Goldsmiths, University of London",
+		"img_src": "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/46/f14f803cf211e5a191b717b8272db1/MOOC-5.png?auto=format&dpr=1&h=72&w=72&fit=fill&bg=FFF",
+		"url": "https://www.coursera.org/learn/responsive-website-examples/home/welcome"
+	},
+	{
+		"name": "Ruby on Rails: An Introduction",
+		"uni": "Johns Hopkins University",
+		"img_src": "https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://coursera-course-photos.s3.amazonaws.com/01/6bfd203bb211e5badc79dbdaa0a51c/jhep-coursera-course1.png?auto=format&dpr=1&h=72&w=72&fit=fill&bg=FFF",
+		"url": "https://www.coursera.org/learn/ruby-on-rails-intro/home/welcome"
+	}
+];
--- responsive-website-development-and-design/1-responsive-website-basics/4-week-res-web-dev-des/js/main.js
+++ responsive-website-development-and-design/1-responsive-website-basics/4-week-res-web-dev-des/js/main.js
@@ -14,14 +14,14 @@ $(function() {
         var link = $(this).data("link");
         // When any link in menu nav is clicked open its content
         // Get template by id
-        var template = document.getElementById(link + "_templ");
+        var template = document.getElementById(link + "_template");
         // Pass template html data to function so that it is shown
         // to user
         changeContent(template);
     });
 
     // Simulate that first link in menu to be clicked on load
-    $(".nav-pills").find("li:nth-child(2) a").trigger("click");
+    $(".nav-pills").find("li:first-child a").trigger("click");
 
     // Get element by its id
     var select_questions = document.getElementById("questions");
@@ -50,26 +50,58 @@ $(function() {
         // Works the same way as if statement - if user chose question one
         // then show this answer
         switch ($option_value) {
-            case 0:
+            case $option_value:
                 $my_answer.html(data[$option_value].answer);
                 break;
-            case 1:
-                $my_answer.html(data[$option_value].answer);
-                break;
-            case 2:
-                $my_answer.html(data[$option_value].answer);
-                break;
-            case 3:
-                $my_answer.html(data[$option_value].answer);
-                break;
-            case 4:
-                $my_answer.html(data[$option_value].answer);
-                break;
-            case 5:
-                $my_answer.html(data[$option_value].answer);
-                break;
+            default: 
+            	$my_answer.html(data[0].answer);
+            	break;
         }
     });
+
+    // This crazy function creates a list of courses in profile template
+    // It is not that crazy at it seems, just too much writing
+    function load_courses_list(){
+    	// var media_list = document.getElementDyId("media-list");
+    	var list_element, media_left, media_object, media_body, media_heading, media_description, media_link;
+
+    	for(var i = 0; i < courses.length; i++){
+    		list_element = document.createElement("li");
+    		list_element.className = "media thumbnail";
+
+    		media_left = document.createElement("div");
+    		media_left.className = "media-left";
+
+    		media_object = document.createElement("img");
+    		media_object.className = "media-object";
+    		media_object.setAttribute("src", courses[i].img_src);
+    		media_object.setAttribute("alt", courses[i].name);
+
+    		media_body = document.createElement("div");
+    		media_body.className = "media-body media-middle";
+
+    		media_heading = document.createElement("h4");
+    		media_heading.className = "media-heading";
+    		media_heading.innerHTML = courses[i].name;
+
+    		media_description = document.createElement("span");
+    		media_description.innerHTML = courses[i].uni;
+
+    		media_link = document.createElement("a");
+    		media_link.setAttribute("href", "#");
+    		media_link.setAttribute("data-link", courses[i].url)
+
+    		media_list.appendChild(list_element);
+    		list_element.appendChild(media_left);
+    		media_left.appendChild(media_object);
+    		list_element.appendChild(media_body);
+    		media_body.appendChild(media_heading);
+    		media_body.appendChild(media_description);
+    		media_left.appendChild(media_link);
+    	}
+    }
+
+    load_courses_list();
 
     // Simulate .media.thumbnail div to act like a link
     $(".media.thumbnail").click(function(){
