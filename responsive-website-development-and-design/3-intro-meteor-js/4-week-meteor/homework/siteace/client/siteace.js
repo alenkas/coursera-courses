12775169a5391fb6b81cc9b8abd5f81c74498e82
Alenka <ei@everblood.org>
2016-04-19 17:50:09 +0300

    First four tasks complated

---
+++ responsive-website-development-and-design/3-intro-meteor-js/4-week-meteor/homework/siteace/client/siteace.js
@@ -0,0 +1,139 @@
+/// Routing
+
+Router.configure({
+	layoutTemplate: "ApplicationLayout"
+});
+
+Router.route('/', function(){
+	this.render('navbar', {
+		to: "navbar"
+	});
+	this.render('main', {
+		to: "main"
+	});
+});
+
+Router.route('/:_id', function(){
+	this.render('navbar', {
+		to: "navbar"
+	});
+	this.render('website_item', {
+		to: "main",
+		data: function(){
+			return Websites.findOne({ _id: this.params._id });
+		}
+	});
+});
+
+/// Accounts config
+// Display email and username fields
+Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_EMAIL"
+});
+
+/// Comments config
+
+// Comments.ui.config({
+// 	template: 'bootstrap'
+// });
+
+/////
+// template helpers 
+/////
+
+// helper function that returns all available websites
+Template.website_list.helpers({
+    websites: function() {
+        return Websites.find({}, {
+        	sort: {
+        		rating: -1
+        	}
+        });
+    }
+});
+
+Template.website_item.helpers({
+	getCreationDate: function(website_id){
+    	var website = Websites.findOne({ _id: website_id });
+    	if(website){
+    		return website.createdOn;
+    	} else {
+    		return null;
+    	}
+    }
+});
+
+
+/////
+// template events 
+/////
+
+Template.website_item.events({
+    "click .js-upvote": function(event) {
+        // example of how you can access the id for the website in the database
+        // (this is the data context for the template)
+        var website_id = this._id;
+        var rating = this.rating;
+        //Rating is showing before it is changed actually.
+        console.log("upvoted " + rating);
+        console.log("Up voting website with id " + website_id);
+        // put the code in here to add a vote to a website!
+
+        Websites.update({ _id: website_id }, { $set: { rating: rating + 1 }});
+
+
+        return false; // prevent the button from reloading the page
+    },
+    "click .js-downvote": function(event) {
+
+        // example of how you can access the id for the website in the database
+        // (this is the data context for the template)
+        var website_id = this._id;
+        var rating = this.rating;
+        //Rating is showing before it is changed actually.
+        console.log("old rating " + rating);
+        console.log("Down voting website with id " + website_id);
+
+        // put the code in here to remove a vote from a website!
+
+        Websites.update({ _id: website_id }, { $set: { rating: rating - 1 }});
+        return false; // prevent the button from reloading the page
+    }
+})
+
+Template.website_form.events({
+    "click .js-toggle-website-form": function(event) {
+        $("#website_form").toggle('slow');
+    },
+    "submit .js-save-website-form": function(event) {
+    	var url, title, description;
+    	console.log(event.target);
+
+        // here is an example of how to get the url out of the form:
+        url = event.target.url.value;
+        title = event.target.title.value;
+        description = event.target.description.value;
+        console.log("The url they entered is: " + url);
+
+        //  put your website saving code in here!	
+
+        if(Meteor.user()){
+        	Websites.insert({
+        		title: title,
+        		url: url,
+        		description: description,
+        		createdOn: new Date(),
+        		createdBy: Meteor.user()._id
+        	});
+        }
+
+        return false; // stop the form submit from reloading the page
+
+    }
+});
+
+Template.website_item.events({
+	'click .js-click-item': function(){
+		// return false;
+	}
+});
---
+++ responsive-website-development-and-design/3-intro-meteor-js/4-week-meteor/homework/siteace/server/startup.js
@@ -0,0 +1,76 @@
+var websites = [{
+    title: "Goldsmiths Computing Department",
+    url: "http://www.gold.ac.uk/computing/",
+    description: "This is where this course was developed.",
+    createdOn: new Date(),
+    rating: 0
+}, {
+    title: "University of London",
+    url: "http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
+    description: "University of London International Programme.",
+    createdOn: new Date(),
+    rating: 0
+}, {
+    title: "Coursera",
+    url: "http://www.coursera.org",
+    description: "Universal access to the world’s best education.",
+    createdOn: new Date(),
+    rating: 0
+}, {
+    title: "Google",
+    url: "http://www.google.com",
+    description: "Popular search engine.",
+    createdOn: new Date(),
+    rating: 0
+}];
+
+// start up function that creates entries in the Websites databases.
+Meteor.startup(function() {
+    
+        // code to run on server at startup
+        if (!Websites.findOne()) {
+            console.log("No websites yet. Creating starter data.");
+            for (var i = 0; i < websites.length; i++) {
+            console.log(websites[i]);
+            Websites.insert({
+                title: websites[i].title,
+                url: websites[i].url,
+                description: websites[i].description,
+                createdOn: websites[i].createdOn,
+                rating: websites[i].rating
+            });
+
+        }
+        
+        // Websites.insert({
+        //     title: "Goldsmiths Computing Department",
+        //     url: "http://www.gold.ac.uk/computing/",
+        //     description: "This is where this course was developed.",
+        //     createdOn: new Date(),
+        //     rating: 0
+        // });
+        // Websites.insert({
+        //     title: "University of London",
+        //     url: "http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
+        //     description: "University of London International Programme.",
+        //     createdOn: new Date(),
+        //     rating: 0
+        // });
+        // Websites.insert({
+        //     title: "Coursera",
+        //     url: "http://www.coursera.org",
+        //     description: "Universal access to the world’s best education.",
+        //     createdOn: new Date(),
+        //     rating: 0
+        // });
+        // Websites.insert({
+        //     title: "Google",
+        //     url: "http://www.google.com",
+        //     description: "Popular search engine.",
+        //     createdOn: new Date(),
+        //     rating: 0
+        // });
+    }
+});
+
+
