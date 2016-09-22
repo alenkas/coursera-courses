e97e3cbfac8e6b3ad9a1ba671dcebe68aecf61fe
alenka <ei@liquidgears.net>
2016-09-22 14:33:30 +0300

    Lecture 17. ng-repeat

--- fullstack-ruby/single-page-web-app-angular/module2-angular/app.js
+++ fullstack-ruby/single-page-web-app-angular/module2-angular/app.js
@@ -1,29 +1,45 @@
 (function(){
 'use strict';
 
-angular.module('BindingApp', [])
-.controller('BidingController', BidingController);
+var shoppingList1 = [
+	"Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol",
+	"Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"	
+];
 
-BidingController.$inject = ['$scope'];
-	function BidingController($scope) {
-		$scope.firstName = "Yaakov";
-		$scope.fullName = "";
-
-		$scope.showNumbersOfWatchers = function(){
-			console.log("# of Watchers: ", $scope.$$watchersCount);
-		};
-
-		$scope.setFullName = function(){
-			$scope.fullName = $scope.firstName + " " + "Chaikin";
-		};
-
-		$scope.logFirstName = function(){
-			console.log("First name is: ", $scope.firstName);
-		};
-		
-		$scope.logFullName = function(){
-			console.log("Full name is :", $scope.fullName);
-		}	
+var shoppingList2 = [
+	{
+		name: "Milk",
+		quantity: "2"
+	}, {
+		name: "Donuts",
+		quantity: "200"
+	}, 
+	{
+		name: "Cookies",
+		quantity: "300"
+	},
+	{
+		name: "Chocolate",
+		quantity: "5"	
 	}
-	
+];
+
+angular.module('ShoppingListApp', [])
+.controller('ShoppingListController', ShoppingListController);
+
+ShoppingListController.$inject = ['$scope'];
+	function ShoppingListController($scope) {
+		$scope.shoppingList1 = shoppingList1;
+		$scope.shoppingList2 = shoppingList2;
+
+		$scope.addToList = function(){
+			var newItem  = {
+				name: $scope.newItemName,
+				quantity: $scope.newItemQuantity
+			}
+
+			$scope.shoppingList2.push(newItem);
+		}
+	}
+
 })();
